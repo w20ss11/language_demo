1fff2c6e428725c935ce8150ee0383ecd21e1d2e
Your Name <you@example.com>
2022-06-15 21:25:22 +0800

    modify

---
+++ java/com/wss/test/Test.java
@@ -0,0 +1,11 @@
+package com.wss.test;
+
+import com.wss.tools.Utils;
+
+class TEST{
+	Utils utils;
+	
+	public static void main(String[] args){
+		System.out.print("hello word java");
+	}
+}
---
+++ java/com/wss/tools/Utils.java
@@ -0,0 +1,5 @@
+package com.wss.tools;
+
+class Utils{
+	
+}
